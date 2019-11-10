19b8eaa3b4f72048b071b83d5730b4ff3c8b8320
Kreiselman <jkreiselman@wpi.edu>
2019-11-10 17:13:13 -0500

    scene switcher

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public static class SceneSwitcher
+{
+
+    public enum Scene
+    {
+        GameScene,
+    }
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -64,6 +64,7 @@ public class Snake : MonoBehaviour
         else
         {
             Debug.Log("GameOver");
+            SceneSwitcher.Load(SceneSwitcher.Scene.GameScene);
         }
         
         
