4a384bddb2775ca142882e9c7efb63a5f1d78cf1
Brian-Earl <43723279+Brian-Earl@users.noreply.github.com>
2019-11-10 15:57:34 -0500

    Optimization and Better generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -20,20 +20,24 @@ public class MapGenerator : MonoBehaviour
     {
         map = new int[18 * width, 18 * width];
         int[] rooms = new int[num_rooms];
+        for (int i = 0; i < rooms.Length; i++)
+            rooms[i] = -1;
 
-        for (int i = 0; i < num_rooms; i++)
-        { 
-            int num = Random.Range(0, map.Length / (18 * 18));
+            for (int i = 0; i < num_rooms; i++)
+        {
+            int num = Random.Range(0, (width * width));
             while (true)
             {
                 bool isIn = false;
                 foreach (int element in rooms)
+                {
                     if (element == num)
                         isIn = true;
+                }
                 if (!isIn)
                     break;
 
-                num = Random.Range(0, map.Length / (18 * 18));
+                num = Random.Range(0, (width*width));
             }
             rooms[i] = num;
             Debug.Log("Random Num: " + rooms[i]);
@@ -51,64 +55,72 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
-        for (int i = 0; i < (18 * width); i++)
-        {
-            Instantiate(wall, new Vector3(i, -1, 0), Quaternion.identity);
-            Instantiate(wall, new Vector3(i, (18 * width), 0), Quaternion.identity);
-            Instantiate(wall, new Vector3(-1,i, 0), Quaternion.identity);
-            Instantiate(wall, new Vector3((18 * width),i, 0), Quaternion.identity);
-        }
-
+        GameObject border = Instantiate(wall, new Vector3(((18*width)/2)-.5f, -1, 0), Quaternion.identity);
+        border.transform.localScale = new Vector3((18 * width)+2, 1);
+        border = Instantiate(wall, new Vector3(((18 * width) / 2) - .5f, (18 * width), 0), Quaternion.identity);
+        border.transform.localScale = new Vector3((18 * width) + 2, 1);
+        border = Instantiate(wall, new Vector3(-1, ((18 * width) / 2) - .5f, 0), Quaternion.identity);
+        border.transform.localScale = new Vector3(1, 18 * width);  
+        border = Instantiate(wall, new Vector3((18 * width), ((18 * width) / 2) - .5f, 0), Quaternion.identity);
+        border.transform.localScale = new Vector3(1, 18 * width);  
 
         MakeHallways(rooms);
 
         string line = "";
         for (int i = 0; i < (18 * width); i++)
         {
+            GameObject placeWall;
             for (int j = 0; j < (18 * width); j++)
             {
-                if (map[i, j] == 0)
-                {
-                    Instantiate(wall, new Vector3(i, j, 0), Quaternion.identity);
-                }
-                else
+                if(map[j, i] != 0)
                 {
                     int[] cords = new int[2];
-                    cords[0] = i;
-                    cords[1] = j;
+                    cords[0] = j;
+                    cords[1] = i;
                     valid_spots.Add(cords);
+                    if (j < (18 * width) && map[j + 1, i] == 0)
+                        placeWall = Instantiate(wall, new Vector3(j + 1, i, 0), Quaternion.identity);
+                    if (j > 0 && map[j - 1, i] == 0)
+                        placeWall = Instantiate(wall, new Vector3(j - 1, i, 0), Quaternion.identity);
+                    if (i < (18 * width) && map[j, i + 1] == 0)
+                        placeWall = Instantiate(wall, new Vector3(j, i + 1, 0), Quaternion.identity);
+                    if (i > 0 && map[j, i - 1] == 0)
+                        placeWall = Instantiate(wall, new Vector3(j , i - 1, 0), Quaternion.identity);
                 }
-                line += string.Format("{0}", map[i, j]);
+                line += string.Format("{0}", map[j, i]);
             }
             line += System.Environment.NewLine;
         }
         Debug.Log(line);
-        AddFood();
+        for (int i = 0; i < num_food; i++)
+            AddFood();
     }
 
     public void AddFood()
     {
-        for (int i = 0; i < num_food; i++)
-        {
             int[] spot = (int[])(valid_spots[Random.Range(0, valid_spots.Count)]);
             Instantiate(food, new Vector3(spot[0], spot[1], 0), Quaternion.identity);
-        }
     }
 
     private void MakeRoom(int x, int y)
     {
-        int roomWidth = Random.Range(14, 18);
-        int roomHeight = Random.Range(14, 18);
+        int roomWidth = Random.Range(13, 17);
+        while(roomWidth % 2 == 0)
+            roomWidth = Random.Range(13, 17);
+        int roomHeight = Random.Range(13, 17);
+        while(roomHeight % 2 == 0)
+            roomHeight = Random.Range(13, 17);
         int mapX = x * 18;
         int mapY = y * 18;
 
-        for (int i = 0; i < roomHeight - 0; i++)
+        GameObject floor_inst = Instantiate(floor, new Vector3(mapX + roomWidth/2, mapY + roomHeight/2, 0), Quaternion.identity);
+        floor_inst.GetComponent<SpriteRenderer>().color = new Color(colors[current_color].g / 255, colors[current_color].r / 255, colors[current_color].b / 255);
+        floor_inst.transform.localScale = new Vector3(roomWidth, roomHeight);
+        for(int i = 0; i < roomHeight;i++)
         {
-            for (int j = 0; j < roomWidth - 0; j++)
+            for(int j = 0; j < roomWidth; j++)
             {
                 map[j + mapX, i + mapY] = 1;
-                GameObject floor_inst = Instantiate(floor, new Vector3(j + mapX, i + mapY, 0), Quaternion.identity);
-                floor_inst.GetComponent<SpriteRenderer>().color = new Color(colors[current_color].g / 255, colors[current_color].r / 255, colors[current_color].b / 255);
             }
         }
         current_color++;
@@ -141,8 +153,11 @@ public class MapGenerator : MonoBehaviour
         int index = before;
         for (int p = 0; p < 2; p++)
         {
+            GameObject hallway;
             if ((index / width) * 18 < (current / width) * 18)
             {
+                //hallway = Instantiate(floor, new Vector3(j + ((current % width) * 18), k, 0), Quaternion.identity);
+                //hallway.transform.localScale = new Vector3(0, 2);
                 for (int k = (index / width) * 18; k < 1 + (current / width) * 18; k++)
                 {
                     for (int j = 0; j < 2; j++)
